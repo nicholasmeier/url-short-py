5ad6359fb89a2fb9ef2a04e8fca177cab98d2745
nicholasmeier <nimtizmeier@gmail.com>
2022-06-23 09:42:13 -0400

    simple script version with existing apis

--- url-short.py
+++ url-short.py
@@ -2,29 +2,34 @@
 import hashlib, base64
 import random
 import pyshorteners
+import sys
 
+#
+# Script version for shortening urls using existing apis / services
+# Limitations: 
+#   - relies on external resources not effective for internal/private endpoints 
+#   - no control over external apis
+# Benefits:
+#   - less developer overhead
+#   - simple code
+#   - No need for a whole stack for existing services
+#
+# 
 
-def gen_short_url(long_url):
-    #init
-    short_url = ""
-    noise = ""
+def main():
+    if len(sys.argv) == 2:
+        long_url = sys.argv[1]
 
-    #make sure short url doesn't already exist
-    do:
-        short_url = hashlib.((sha1long_url + noise).encode("UTF-8")).hexdigest()
-        short_url = short_url[:7]
-        noise = noise + random.choice("qwertyuiopasdfghjklzxcvbnm1234567890")
-    while (check_db(short_url))
-    
-    # temp line change later for actual db
-    db.add(long_url, short_url)
-    
-    return baseuri + short_url
+        shortener_tu = pyshorteners.Shortener()
+        short_url = shortener_tu.tinyurl.short(long_url)
 
-
-# just an outline, use real vars + db later
-def check_db(short_url):
-    if (short_url in db.short_urls):
-        return True
+        print("Short URL = " + short_url)
+    elif len(sys.argv) == 1:
+        long_url = input("Enter long url: ")
     else:
-        return False
+        print("err: incorrect format")
+        print("example usage: ./url-short.py <longurl>")
+        exit(1)
+
+if __name__ == "__main__":
+    main()
