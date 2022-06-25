224e7c0b0d9960c85cf9244398e86c1baa1f0ff1
nicholasmeier <nimtizmeier@gmail.com>
2022-06-25 09:14:43 -0400

    added a bunch of db related function from flask sqlalchemy

--- url-short.py
+++ url-short.py
@@ -1,14 +1,20 @@
 #!/bin/python3
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 import hashlib, base64
 import random
 
+#short_url = db
+#shorturl = appcode
+
+
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////url-shortener.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/url-shortener.db'
 db = SQLAlchemy(app)
 
-class UrlDBE(db.model):
+
+#URL DB class
+class UrlDBE(db.Model):
     short_url = db.Column("shorturl", db.String(7), primary_key=True)
     long_url = db.Column("longurl", db.String())
     
@@ -16,49 +22,66 @@ class UrlDBE(db.model):
         self.short_url = short_url
         self.long_url = long_url
 
+#init db
+@app.before_first_request
+def create_tables():
+    db.create_all()
+
+# index html page
 @app.route('/', methods=['POST', 'GET'])
 def index():
     if (request.method == "POST"):
-        long_url = request.form["long_url_in"]
-        # do more here
-    if (request.method == "GET"):
-        return render_template('index.html')
+        longurl = request.form["long_url_in"]
+        shorturl = gen_short_url(longurl)
+        
+        # Conditional for db check timeout
+        if shorturl == -1:
+            print("error with url gen")
+            return render_template('index.html', display_shorturl="Err: Could not generate shortURL")
+        newurlpair = UrlDBE(shorturl, longurl)
+        db.session.add(newurlpair)
+        db.session.commit()
+        return render_template('index.html', display_shorturl=shorturl)
+    elif (request.method == "GET"):
+        return render_template('index.html', display_shorturl=" ")
 
-def gen_short_url(long_url):
-    #init
-    short_url = ""
+@app.route('/<shorturl>')
+def redirect_shorturl(shorturl):
+    longurl = UrlDBE.query.filter_by(short_url=shorturl).first()
+    if longurl:
+        return "<head><title>Redirecting...</title><meta http-equiv=\"refresh\" content=\"0; url=" + long_url + "\"/></head><body><a>Redirecting</a>"
+    else:
+        return "Err: Broken link, sorry :("
+
+
+def gen_short_url(longurl):
+    shorturl = ""
     noise = ""
     #make sure short url doesn't already exist
     # is there a better way to do this? probably
-    do:
-        short_url = hashlib.sha1((long_url + noise).encode("UTF-8")).hexdigest()[:7]
+    # gonna make this hacky and not infinitely loop
+    infinitybreaker = 0
+    while(True):
+        shorturl = hashlib.sha1((longurl + noise).encode("UTF-8")).hexdigest()[:7]
+        if ( (bool(UrlDBE.query.filter_by(short_url=shorturl).first())) == False):
+            return shorturl
         noise = noise + random.choice("qwertyuiopasdfghjklzxcvbnm1234567890")
-    while (check_db(short_url))
-    
-    # temp line change later for actual db
-    db.add(long_url, short_url)
-    
-    return short_url
+        infinitybreaker += 1
+        if infinitybreaker == 600:
+            break
+    #while (bool(UrlDBE.query.filter_by(short_url=shorturl).first()))
+    #return short_url
+    return -1
 
-
-#
-# Name : generate_html
-# Desc : Generate html with a built in redirect with given url
-# Returns : string of html 
-#
-def generate_html(long_url):
-    return"<html><head><title>Redirecting...</title><meta http-equiv=\"refresh\" content=\"0; url=" + long_url + "\"/></head><body><a>Redirecting</a></html>"
+#def generate_html(long_url):
+    #return"<html><head><title>Redirecting...</title><meta http-equiv=\"refresh\" content=\"0; url=" + long_url + "\"/></head><body><a>Redirecting</a></html>"
     
 # just an outline, use real vars + db later
-#def check_db(short_url):
-#    if (short_url in db.short_urls):
-#        return True
-#    else:
-#        return False
-
-
-
-
+#def check_db(short_url,):
+ #   if (short_url in db.short_urls):
+  #      return True
+   # else:
+    #    return False
 
 
 
