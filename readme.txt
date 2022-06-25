bf71440264d361c7f1dca54b2a28360e63d3d1cc
nicholasmeier <nimtizmeier@gmail.com>
2022-06-25 10:21:31 -0400

    beautifing code and updating readme

--- readme.txt
+++ readme.txt
@@ -2,12 +2,23 @@
 # Experimenting with Flask
 # originally wanted to do this with AWS lambda and DynamoDB -> basically no time for that
 
+# Local Run instructions:
+# pip install Flask Flask-SQLAlchemy
+# python3 url-short.py
+# connect to localhost:5000
+# type in urls
+
+
+
 # Dev plan:
 #X  Basic code for generating unique url
 #X  maybe experiment with existing apis for url stuff --> see pythonscript-existing-api branch
-#/  make some front end basic html
-# Experimenting with FLASK and FlaskSQLAlchemy
-# add a db / connection
+#X  make some front end basic html
+#X Experimenting with FLASK and FlaskSQLAlchemy
+#X  add a db / connection
+
+#functional program completed now need to start hosting
+# also add howto run for local code
 
 
 # final write up for future dev / plan resources etc
--- url-short.py
+++ url-short.py
@@ -4,8 +4,8 @@ from flask_sqlalchemy import SQLAlchemy
 import hashlib, base64
 import random
 
-#short_url = db
-#shorturl = appcode
+#short_url = db_var
+#shorturl = app_var
 
 
 app = Flask(__name__)
@@ -13,7 +13,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/url-shortener.db'
 db = SQLAlchemy(app)
 
 
-#URL DB class
+#URL DB object
 class UrlDBE(db.Model):
     short_url = db.Column("shorturl", db.String(7), primary_key=True)
     long_url = db.Column("longurl", db.String())
@@ -32,16 +32,29 @@ def create_tables():
 def index():
     if (request.method == "POST"):
         longurl = request.form["long_url_in"]
+
+        #Conditionals for formatting longurl:
+        if (longurl[:4] == "www."):
+            longurl = "https://" + longurl
+        elif (longurl[:8] != "https://" and longurl[:7] != "http://"):
+            return render_template('index.html', display_shorturl="Err: check formatting for your url input")
+
         shorturl = gen_short_url(longurl)
-        
         # Conditional for db check timeout
         if shorturl == -1:
             print("error with url gen")
             return render_template('index.html', display_shorturl="Err: Could not generate shortURL")
+
+        # create new db entry
         newurlpair = UrlDBE(shorturl, longurl)
         db.session.add(newurlpair)
         db.session.commit()
-        return render_template('index.html', display_shorturl=shorturl)
+        
+        # return shortened link
+        dispshort = request.base_url + shorturl
+        return render_template('index.html', display_shorturl=dispshort)
+
+    # default index
     elif (request.method == "GET"):
         return render_template('index.html', display_shorturl=" ")
 
@@ -50,8 +63,6 @@ def redirect_shorturl(shorturl):
     urldb_entry = UrlDBE.query.filter_by(short_url=shorturl).first()
     if urldb_entry:
         return redirect(urldb_entry.long_url)
-        #return render_template('redirect.html', redirectContent=contentstring)
-        #return "<head><title>Redirecting...</title><meta http-equiv=\"refresh\" content=\"0; url=" + urldb_entry.long_url + "\"/></head><body><a>Redirecting</a>"
     else:
         return "Err: Broken link, sorry :("
 
@@ -59,33 +70,24 @@ def redirect_shorturl(shorturl):
 def gen_short_url(longurl):
     shorturl = ""
     noise = ""
-    #make sure short url doesn't already exist
-    # is there a better way to do this? probably
-    # gonna make this hacky and not infinitely loop
+    # is there a better way to do this? maybe?
     infinitybreaker = 0
     while(True):
+        #generate shorturl from hashfunction
         shorturl = hashlib.sha1((longurl + noise).encode("UTF-8")).hexdigest()[:7]
+
+        #check if hashfunction already exists in db
         if ( (bool(UrlDBE.query.filter_by(short_url=shorturl).first())) == False):
+            #if not then just return
             return shorturl
+        #else create some noise
         noise = noise + random.choice("qwertyuiopasdfghjklzxcvbnm1234567890")
+
+        #make sure this only runs for x loops
         infinitybreaker += 1
         if infinitybreaker == 600:
             break
-    #while (bool(UrlDBE.query.filter_by(short_url=shorturl).first()))
-    #return short_url
     return -1
-
-#def generate_html(long_url):
-    #return"<html><head><title>Redirecting...</title><meta http-equiv=\"refresh\" content=\"0; url=" + long_url + "\"/></head><body><a>Redirecting</a></html>"
-    
-# just an outline, use real vars + db later
-#def check_db(short_url,):
- #   if (short_url in db.short_urls):
-  #      return True
-   # else:
-    #    return False
-
-
 
 if (__name__ == '__main__'):
     app.run(port=5000, debug=True)
